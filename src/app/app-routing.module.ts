deb55ec1794f7e7b5381ee8f8660b1e074493274
Sophie Borges Da Silva <sophieborges@brq.com>
2022-11-08 16:40:33 -0300

    adicionando componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { FilmesComponent } from './filmes/filmes.component';
+import { GeneroComponent } from './genero/genero.component';
+import { HeaderComponent } from './header/header.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'usuario', component: UsuarioComponent},
+  {path: 'filmes', component: FilmesComponent},
+  {path: 'genero', component: GeneroComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ReactiveFormsModule} from '@angular/forms';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { HomeComponent } from './home/home.component';
+import { FilmesComponent } from './filmes/filmes.component';
+import { GeneroComponent } from './genero/genero.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    UsuarioComponent,
+    HomeComponent,
+    FilmesComponent,
+    GeneroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
